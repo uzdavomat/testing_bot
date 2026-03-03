d645e739d4b0e25eab9853d7754b9bae573d021a
uzdavomat <uzdavomat@gmail.com>
2026-03-03 17:33:17 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import asyncio
+from aiogram import Bot,Dispatcher
+from  aiogram.types import Message
+from aiogram.filters import CommandStart
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+TOKEN = os.getenv("TOKEN")
+
+bot= Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("Hello there")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+
+
